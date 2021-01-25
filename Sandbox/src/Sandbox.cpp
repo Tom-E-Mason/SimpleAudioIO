7013f6f3aaa467418d27cc21f61bc8e1f50ff5d8
Tom Mason <masonworldt@gmail.com>
2021-01-25 15:59:37 +0000

    Lets user choose their input and output devices to hear themselves back through.

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,7 +1,34 @@
 
-#include <IO.h>
+#include "IO.h"
 
 int main()
 {
+    IO io;
 
+    auto devs{ io.GetInputDevices() };
+    std::cout << "Available input devices:\n\n";
+    for (size_t i{ 0 }; i < devs.size(); ++i)
+        std::wcout << "    " << i + 1 << ": " << devs.at(i) << "\n";
+    
+    std::cout << "\nSelect by number.\n\n";
+    
+    size_t uDeviceId{ 0 };
+    std::cin >> uDeviceId;
+    --uDeviceId;
+
+    io.SetInputAudioDevice(devs.at(uDeviceId));
+    
+    devs = io.GetOutputDevices();
+    std::cout << "\nAvailable output devices:\n\n";
+    for (size_t i{ 0 }; i < devs.size(); ++i)
+        std::wcout << "    " << i + 1 << ": " << devs.at(i) << "\n";
+    
+    std::cout << "\nSelect by number.\n\n";
+    
+    std::cin >> uDeviceId;
+    --uDeviceId;
+
+    io.SetOutputAudioDevice(devs.at(uDeviceId));
+    
+    io.Play();
 }
